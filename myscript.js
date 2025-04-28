c7106d9ef3b659a1f21ca84f2dd2b149fcef58f6
Narayan7330 <narayanmali7330@gmail.com>
2025-04-28 05:07:05 +0000

    added loading in starting

---
+++ myscript.js
@@ -0,0 +1,10 @@
+// script.js
+window.addEventListener("load", function () {
+  const loader = document.getElementById("loader");
+  const content = document.getElementById("content");
+
+  setTimeout(() => {
+    loader.style.display = "none";
+    content.style.display = "block";
+  }, 2000); // 2 seconds delay
+});
